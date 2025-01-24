14b1c4af6e09d9a373a87d43c16b88817a1f6a35
shjk0531 <shinjk0531@gmail.com>
2025-01-24 14:15:18 +0900

    feature main container position

--- src/pages/main/page/MainPage.tsx
+++ src/pages/main/page/MainPage.tsx
@@ -1,12 +1,14 @@
 import '../ui/MainPageStyle.css';
 import React, { useEffect, useState } from 'react';
 import { SideBar } from '../../../widgets';
-import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
 
 const MainPage: React.FC = () => {
     return (
         <>
-            <SideBar />
+            <div className="map-container"></div>
+            <div className="menu-container">
+                <SideBar />
+            </div>
             <div></div>
             <div></div>
         </>
---
+++ src/widgets/sideContainer/component/sideContainer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const sideContainer: React.FC = () => {
+    return (
+        <Container>
+            <div className="">
+                <div></div>
+            </div>
+        </Container>
+    );
+};
+
+export default sideContainer;
