77bce71fadd18c0c00d11f843003443dcd85aa90
suuny0321 <joosun7647@naver.com>
2025-02-03 03:49:03 +0900

    리뷰어 랭킹 페이지 및 스타일 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-export { default as MainPage } from './main/page/MainPage';
-export { default as MyPage } from './myPage/page/MyPage';
-export { default as SignUpPage } from './signUp/page/SignUpPage';
-export { default as LoginPage } from './loginPage/page/LoginPage';
+export { default as MainPage } from "./main/page/MainPage";
+export { default as MyPage } from "./myPage/page/MyPage";
+export { default as SignUpPage } from "./signUp/page/SignUpPage";
+export { default as LoginPage } from "./loginPage/page/LoginPage";
+export { default as RankingPage } from "./ranking/page/RankingPage";
