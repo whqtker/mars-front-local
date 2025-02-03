ca85b56594a08c1fa7e6059ff0fd1dacbc49fb42
shjk0531 <shinjk0531@gmail.com>
2025-02-03 14:44:26 +0900

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { NavigationBar } from "../widgets/index";
-import "../index";
-import "./ui/App.css";
-import { pathItemMap } from "../shared/pathItems/pathItems";
-import { MainPage, MyPage, SignUpPage, LoginPage, RankingPage } from "../pages";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { NavigationBar } from '../widgets/index';
+import '../index';
+import './ui/App.css';
+import { pathItemMap } from '../shared/pathItems/pathItems';
+import { MainPage, MyPage, SignUpPage, LoginPage, RankingPage } from '../pages';
 
 const App: React.FC = () => {
   const Ranking: React.FC = () => <div>리뷰어 순위</div>;
@@ -22,22 +22,34 @@ const App: React.FC = () => {
         <div className="flex flex-1">
           <div className="custom-main-container flex-1">
             <Routes>
-              <Route path={pathItemMap["home"].path} element={<MainPage />} />
+                            <Route
+                                path={pathItemMap['home'].path}
+                                element={<MainPage />}
+                            />
+                            <Route
+                                path={pathItemMap['ranking'].path}
+                                element={<RankingPage />}
+                            />
               <Route
-                path={pathItemMap["ranking"].path}
-                element={<RankingPage />}
+                                path={pathItemMap['login'].path}
+                                element={<LoginPage />}
               />
-              <Route path={pathItemMap["login"].path} element={<LoginPage />} />
               <Route
-                path={pathItemMap["signUp"].path}
+                                path={pathItemMap['signUp'].path}
                 element={<SignUpPage />}
               />
               <Route
-                path={pathItemMap["community"].path}
+                                path={pathItemMap['community'].path}
                 element={<Community />}
               />
-              <Route path={pathItemMap["myPage"].path} element={<MyPage />} />
-              <Route path="/recommendation" element={<Recommendation />} />
+                            <Route
+                                path={pathItemMap['myPage'].path}
+                                element={<MyPage />}
+                            />
+                            <Route
+                                path="/recommendation"
+                                element={<Recommendation />}
+                            />
             </Routes>
           </div>
         </div>
