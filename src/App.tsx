4b0c77f9b23726c395e38f7fcb389715607a5243
shjk0531 <shinjk0531@gmail.com>
2025-02-06 17:10:57 +0900

    fix tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,6 @@ import {
     RecommendedRestaurantsPage,
     AuthNaverCallback,
 } from './components/pages';
-  MainPage,
-  CommunityPage,
-  PopularReviewersPage,
-  RecommendedRestaurantsPage,
-  RestaurantDetailPage,
-  AuthNaverCallback,
-} from "./components/pages";
 
 function App() {
     return (
