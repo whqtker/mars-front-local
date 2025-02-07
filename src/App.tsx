fe0038d3422448d4734e8589761f779ee7ff5d88
suuny0321 <joosun7647@naver.com>
2025-02-07 12:48:33 +0900

    PostDetail.tsx 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,37 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Header from './components/layout/Header';
-import Sidebar from './components/layout/Sidebar';
-import MapArea from './components/map/MapArea';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/layout/Header";
+import Sidebar from "./components/layout/Sidebar";
+import MapArea from "./components/map/MapArea";
 
-import CommunityPage from './components/pages/community/CommunityPage';
-import PostDetail from './components/pages/community/PostDetail';
-import WriteForm from './components/pages/community/WriteForm';
-import { store } from './utils/store/Store';
-import { Provider } from 'react-redux';
-import React from 'react';
+import CommunityPage from "./components/pages/community/CommunityPage";
+import WriteForm from "./components/pages/community/WriteForm";
+import { store } from "./utils/store/Store";
+import { Provider } from "react-redux";
+import React from "react";
 
 import {
-    MainPage,
-    PopularReviewersPage,
-    RecommendedRestaurantsPage,
-    AuthNaverCallback,
-} from './components/pages';
+  MainPage,
+  PopularReviewersPage,
+  RecommendedRestaurantsPage,
+  AuthNaverCallback,
+} from "./components/pages";
 
 function App() {
-    return (
-        <React.StrictMode>
-            <Provider store={store}>
-                <Router>
-                    <div className="h-full bg-gray-100 flex flex-col">
-                        <Header />
-                        <div className="h-full page-container">
-                            <Routes>
-                                <Route path="/" element={<MainPage />} />
-                                <Route
-                                    path="/recommendations"
-                                    element={<RecommendedRestaurantsPage />}
-                                />
-                                {/* <Route
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <Router>
+          <div className="h-full bg-gray-100 flex flex-col">
+            <Header />
+            <div className="h-full page-container">
+              <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route
+                  path="/recommendations"
+                  element={<RecommendedRestaurantsPage />}
+                />
+                {/* <Route
                                     path="/map"
                                     element={
                                         <div className="relative flex">
@@ -41,33 +40,20 @@ function App() {
                                         </div>
                                     }
                                 /> */}
-                                <Route
-                                    path="/community"
-                                    element={<CommunityPage />}
-                                />
-                                <Route
-                                    path="/community/:id"
-                                    element={<PostDetail />}
-                                />
-                                <Route
-                                    path="/community/writeForm"
-                                    element={<WriteForm />}
-                                />
-                                <Route
-                                    path="/reviewers"
-                                    element={<PopularReviewersPage />}
-                                />
-                                <Route
-                                    path="/auth/naver/callback"
-                                    element={<AuthNaverCallback />}
-                                />
-                            </Routes>
-                        </div>
-                    </div>
-                </Router>
-            </Provider>
-        </React.StrictMode>
-    );
+                <Route path="/community" element={<CommunityPage />} />
+                <Route path="/community/writeForm" element={<WriteForm />} />
+                <Route path="/reviewers" element={<PopularReviewersPage />} />
+                <Route
+                  path="/auth/naver/callback"
+                  element={<AuthNaverCallback />}
+                />
+              </Routes>
+            </div>
+          </div>
+        </Router>
+      </Provider>
+    </React.StrictMode>
+  );
 }
 
 export default App;
